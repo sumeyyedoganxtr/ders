f621297b3b6e7075d2010f19a56bac2cfac7907f
ihsan <sumeyyedoganxtr@gmail.com>
2020-11-20 18:15:20 +0300

    ders

--- src/deneme.java
+++ src/deneme.java
@@ -2,6 +2,6 @@ public class deneme {
     public static void main(String[] args) {
         System.out.println("birinci komut");
 System.out.println("ikinci komut");
-
+        System.out.println("ikinci komut");
     }
 }
